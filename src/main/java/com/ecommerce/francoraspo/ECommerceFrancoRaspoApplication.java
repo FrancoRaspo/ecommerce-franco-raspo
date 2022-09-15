7cc017a9d62d58f5dbcc5cd3a73a858bdb2e4c46
francoraspo <francoraspo@gmail.com>
2022-09-15 18:03:55 -0300

    Initial commit

---
+++ src/main/java/com/ecommerce/francoraspo/ECommerceFrancoRaspoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.francoraspo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFrancoRaspoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceFrancoRaspoApplication.class, args);
+	}
+
+}
